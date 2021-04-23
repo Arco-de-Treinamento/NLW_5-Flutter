4abd7623fe9912de3d594ee407d03450f4354b81
JosManoel <josmanoelfreitas2012@gmail.com>
2021-04-23 16:27:58 -0300

    Aula 03 - In Orbit

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -1,10 +1,13 @@
 import 'package:DevQuiz/core/app_gradients.dart';
 import 'package:DevQuiz/core/app_text_styles.dart';
 import 'package:DevQuiz/home/widgets/score_card/score_card_widget.dart';
+import 'package:DevQuiz/shared/models/user_model.dart';
 import 'package:flutter/cupertino.dart';
 
 class AppBarWidget extends PreferredSize {
-  AppBarWidget()
+  final UserModel user;
+
+  AppBarWidget({required this.user})
       : super(
           preferredSize: Size.fromHeight(250),
           child: Container(
@@ -26,7 +29,7 @@ class AppBarWidget extends PreferredSize {
                             style: AppTextStyles.title,
                             children: [
                               TextSpan(
-                                  text: "JosManoel",
+                                  text: user.name,
                                   style: AppTextStyles.titleBold)
                             ])),
                         Container(
@@ -35,8 +38,7 @@ class AppBarWidget extends PreferredSize {
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(10),
                             image: DecorationImage(
-                              image: NetworkImage(
-                                  "https://avatars.githubusercontent.com/u/63619736?s=400&u=469f3475ded0f6d62311d3db781df3ec12b44010&v=4"),
+                              image: NetworkImage(user.photoUrl),
                               fit: BoxFit.cover,
                             ),
                           ),
